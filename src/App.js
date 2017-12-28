50f7647fd3118689e949208c87f6f6918e593e01
Cezar <grigorecezar@yahoo.com>
2017-12-28 16:19:40 +0200

    add pi-api

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={temperatures: []};
+  }
+
+  componentDidMount() {
+    fetch('/get/temperature')
+    .then(res => {
+      return res.json()
+    })
+    .then(temperatures => { 
+      this.setState({ temperatures })
+    });
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Silo Temperature Sensor</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.temperatures.map(temperature =>
+            <div key={temperature.id}>The last read temperature is: <b>{temperature.temp}</b>°C.</div>
+          )} 
         </p>
       </div>
     );
