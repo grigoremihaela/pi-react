2c7f96616fb7a349854bf8d4edcb2b13efd82d3f
Cezar <grigorecezar@yahoo.com>
2017-12-28 16:34:53 +0200

    add text

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Silo Temperature Sensor</h1>
+          <p className="App-intro">by Miha</p>
         </header>
         <p className="App-intro">
           {this.state.temperatures.map(temperature =>
