1d9167671b6b6120a5fbe9bb3479d242c209b224
Cezar <grigorecezar@yahoo.com>
2018-01-04 15:46:45 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo-pi.svg';
 import './App.css';
 
 class App extends Component {
+
   constructor(){
     super();
     this.state ={temperatures: []};
@@ -24,16 +25,17 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Silo Temperature Sensor</h1>
-          <p className="App-intro">by Miha</p>
-          <p className="App-intro">
-            {this.state.temperatures.map(temperature =>
-              <div key={temperature.id}>The last read temperature is: <b>{temperature.temp}</b>°C.</div>
-            )} 
-          </p>
+          <p>by Miha</p>
         </header>
+        <p className="App-intro">
+          {this.state.temperatures.map(temperature =>
+            <div key={temperature.id}>The last read temperature is: <b>{temperature.temp}</b>°C.</div>
+          )} 
+        </p>
       </div>
     );
   }
+
 }
 
 export default App;
